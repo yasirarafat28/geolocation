1716586776c5584fe75de7b1b7819bfcdb15a2aa
Md. Yasir Aarafat <yasirarafat1995@gmail.com>
2022-03-08 20:58:55 +0600

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,50 @@
 
+//! # geolocation
+//! Get IP address geolocation information freely.
+//!
+//! ```
+//! geolocation = "0.1.0"
+//! ```
+//! Add to `Cargo.toml`.
+//!
+//! ## Example
+//! 
+//! Using `geolocation` is really quite easy:
+//! ```
+
+//! use geolocation;
+
+//! fn main() {
+//! let ip = "127.0.0.1";
+    //! let location = geolocation::find(ip).unwrap();
+//! 
+    //! println!("{:?}", location);
+//! }
+//! ```
+//!
+//! This and more examples are found in the examples directory.
+//!
+//! ## Query Limits
+//! You can send 45 requests per minute.
+//!
+//! ## Fields
+//! The API can get these fields about IP addresses.
+//!
+//! - [`ip`](crate::Locator::ip)
+//! - [`latitude`](crate::Locator::latitude)
+//! - [`longitude`](crate::Locator::longitude)
+//! - [`city`](crate::Locator::city)
+//! - [`region`](crate::Locator::region)
+//! - [`country`](crate::Locator::country)
+//! - [`timezone`](crate::Locator::timezone)
+//! - [`location`](crate::Locator::location)
+//!
+//! Written with love, in Rust.
+//!
+//! 
+//! 
 
 use serde_json::Value;
-// use std::fmt;
-// use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-
 use isahc::prelude::*;
 pub struct Locator {
     pub ip: String,
@@ -17,28 +58,21 @@ pub struct Locator {
 }
 
 fn find(ip: Option<&str>)-> Result<Locator, isahc::Error>{
-    // let ip: &str = "103.35.168.86";
-
     let uri = format!("http://ip-api.com/json/{:?}",&ip);
-    // https://ipapi.co/103.35.168.86/json/
-
     println!("URI : {}",uri);
     let mut local_data_response = isahc::get(uri)?;
 
     let local_data = local_data_response.text().unwrap();
     let local_body:Value = serde_json::from_str(&local_data).unwrap() ;
-    println!("{}",&local_body["city"].to_string());
-
     let result = Locator {
-
-            ip:"".to_string(),
-            latitude:"".to_string(),
-            longitude:"".to_string(),
-            city:"".to_string(),
-            region:"".to_string(),
-            country:"".to_string(),
-            timezone:"".to_string(),
-            location:"".to_string(),
+        ip:local_body["query"].to_string(),
+        latitude:local_body["lat"].to_string(),
+        longitude:local_body["lon"].to_string(),
+        city:local_body["city"].to_string(),
+        region:local_body["regionName"].to_string(),
+        country:local_body["country"].to_string(),
+        timezone:local_body["timezone"].to_string(),
+        location:format!("{:?}, {:?}, {:?}",local_body["city"].to_string(),local_body["regionName"].to_string(),local_body["country"].to_string()),
     };
 
     Ok(result)
